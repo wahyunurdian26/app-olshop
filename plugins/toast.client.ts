abb23f748069ca57db11b4a62b911b24f3e35b58
Wahyu Nurdian <wahyunurdian98@gmail.com>
2025-04-24 20:06:34 +0700

    toas issue

--- plugins/toast.client.ts
+++ plugins/toast.client.ts
@@ -1,16 +1,20 @@
-// plugins/toast.client.ts
-import Toast, { type PluginOptions } from 'vue-toastification'
+import Toast, * as ToastModule from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const options: PluginOptions = {
+  const options = {
     timeout: 3000,
     closeOnClick: true,
     pauseOnHover: true,
     draggable: true,
     hideProgressBar: false,
-    
   }
 
   nuxtApp.vueApp.use(Toast, options)
+
+  return {
+    provide: {
+      useToast: ToastModule.useToast
+    }
+  }
 })
