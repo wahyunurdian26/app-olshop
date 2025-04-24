88c0647cd69759a142c42c39a46057ebb67871ae
Wahyu Nurdian <wahyunurdian98@gmail.com>
2025-04-24 20:26:44 +0700

    toas issue

--- pages/auth/callback.vue
+++ pages/auth/callback.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
 import { useRouter, useRoute } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
-import { useToast } from 'vue-toastification'
+const $toast = useToastSafe()
 
 const router = useRouter()
 const route = useRoute()
 const auth = useAuthStore()
-const toast = useToast()
+
 
 const token = route.query.token as string
 
 if (token) {
   auth.login(token)
-  toast.success('Login berhasil dengan Google!')
+  $toast.success('Login berhasil dengan Google!')
   router.push('/dashboard')
 } else {
-  toast.error('Token tidak ditemukan dari Google login!')
+  $toast.error('Token tidak ditemukan dari Google login!')
   router.push('/auth')
 }
 </script>
